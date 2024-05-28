fa6ed401e8556f837df4a01fb5878f6a5612f6a5
Héctor Adrián Flores Núñez <hectorsabac2006@gmail.com>
2024-05-28 14:21:30 -0600

    FUN

--- Semana7P3.cpp
+++ Semana7P3.cpp
@@ -60,7 +60,7 @@ int main() {
 class B {};
 class D : public B {};
 #endif
-#define SVD3
+//#define SVD3
 #ifdef SVD3
 class B {
 public:
@@ -73,5 +73,38 @@ int main(){
     D* d = dynamic_cast<D*>(b); // Compila!
 }
 #endif
-
-SVD3
+#define OTROEJERCICIO
+#ifdef OTROEJERCICIO
+class B {
+public:
+    void fun1() {
+        cout << "base-1\n";
+    }
+    virtual void fun2() {
+        cout << "base-2\n";
+    }
+    virtual void fun3() {
+        cout << "base-3\n";
+    }
+    virtual void fun4() {
+        cout << "base-4\n";
+    }
+};
+class D : public B{
+public:
+    void fun1() {
+        cout << "derived-1\n";
+    }
+    void fun2() {
+        cout << "derived-2\n";
+    }
+    void fun4(int x) {
+        cout << "derived-4\n";
+    }
+};
+int main() {
+    B* p; D obj1; p = &obj1;
+    p->fun1(); p->fun2(); p->fun3(); p->fun4(); /*p->fun4(5); <---- ERROR */ obj1.fun4(5);
+}
+#endif
+OTROEJERCICIO
