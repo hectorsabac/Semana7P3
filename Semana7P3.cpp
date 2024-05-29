def8153a88315d42ce90b2c6dfefab51a6fb6edd
Héctor Adrián Flores Núñez <hectorsabac2006@gmail.com>
2024-05-29 13:29:48 -0600

    typeid ejercicios

--- Semana7P3.cpp
+++ Semana7P3.cpp
@@ -108,3 +108,14 @@ int main() {
 }
 #endif
 OTROEJERCICIO
+
+int main() {
+    int i = 65; char c = 70; float f = 3.1416;
+    cout << "tipo de nombre de int = " << typeid(int).name() << endl;
+    cout << "tipo de nombre de i = " << typeid(i).name() << endl;
+    cout << "tipo de nombre de char = " << typeid(char).name() << endl;
+    cout << "tipo de nombre de c = " << typeid(c).name() << endl;
+    cout << "tipo de nombre de float = " << typeid(float).name() << endl;
+    cout << "tipo de nombre de f = " << typeid(f).name() << endl;
+    return 0;
+}
